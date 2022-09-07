99feaf18a67a853687b6ec00c587dc901507b3d8
Adem Erbas <adem5434e@gmail.com>
2022-09-07 19:49:31 +0200

    react-quiz-app

--- react-advanced-tutorial-and-projects/BeginnerLevelProjects/quiz-app/src/App.tsx
+++ react-advanced-tutorial-and-projects/BeginnerLevelProjects/quiz-app/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import PopUpModal from "./components/PopUpModal";
+import QuizPage from "./components/QuizPage";
+import StartPage from "./components/StartPage";
 
 function App() {
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer position="top-center" />
+      <PopUpModal />
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/questions" element={<QuizPage />} />
+      </Routes>
     </div>
   );
 }
